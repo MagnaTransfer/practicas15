d0011bef76ce19134473618f8a0bc21f84a55d1a
Josu <josuherce94@gmail.com>
2015-06-16 13:40:18 +0200

    Crear, modificar y borrar gestor academicos.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -18,12 +18,14 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var managerController = require('../controllers/managerController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.param('id', managerController.load);
+router.get('/manager', managerController.index);
+router.get('/manager/new', managerController.new);
+router.post('/manager/create', managerController.create);
+router.get('/manager/:id(\\d)/edit', managerController.edit);
+router.put('/manager/:id(\\d)', managerController.update);
+router.delete('/manager/:id(\\d)/delete/', managerController.destroy);
 
 module.exports = router;
