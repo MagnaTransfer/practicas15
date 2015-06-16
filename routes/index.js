09c7963839999db608b6763ebf119bad74408392
Yongliang Zhan <yzhan94@gmail.com>
2015-06-16 11:14:05 +0000

    Crear, modificar y eliminar estudiantes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var studentController = require('../controllers/studentController.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* students */
+router.get('/students/new', studentController.new);
+router.get('/students', studentController.index);
+router.post('/students', studentController.create);
+router.get('/students/:userId(\\d+)/edit', studentController.edit);
+router.put('/students/:userId(\\d+)', studentController.update);
+router.delete('/students/:userId(\\d+)', studentController.destroy);
 
 module.exports = router;
